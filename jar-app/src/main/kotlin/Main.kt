9fdbcd60c4578309586a78472dd01d138d7d87cb
Max Korolev <37272192+Ladsers@users.noreply.github.com>
2020-08-24 18:43:55 +0300

    Added task to build executable jar file.

--- jar-app/src/main/kotlin/Main.kt
+++ jar-app/src/main/kotlin/Main.kt
@@ -1,10 +1,10 @@
-
 val tb = TextBundle()
-val version =  "0.9.0.JVM"
+val version =  "0.9.1.JVM"
 
 fun main(args: Array<String>)
 {
     printHeader()
+    //checkUpdates("jvm")
 
     when {
         args.size > 1 -> Processor.openProcess(args[0], args[1])
