eb16b75eca17e4f52adb8c8f9a70017ac07c8eb8
Max Korolev <37272192+Ladsers@users.noreply.github.com>
2022-09-05 23:45:46 +0300

    Gradle Groovy -> Gradle Kotlin.

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+}
+group = "com.ladsers.passtable"
+version = "22.8.6"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.bouncycastle:bcpkix-jdk15on:1.66")
+}
+
+java.sourceSets["main"].java {
+    srcDir("core/src/main/kotlin")
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<Jar>() {
+    archiveFileName.set("Passtable-${project.version}.jar")
+
+    manifest {
+        attributes(
+            mapOf(
+                "Main-Class" to "com.ladsers.passtable.jvm.MainKt",
+                "Implementation-Title" to project.name,
+                "Kotlin-Version" to kotlin.coreLibrariesVersion,
+                "Implementation-Version" to project.version,
+                "Build-Jdk" to java.targetCompatibility
+            )
+        )
+    }
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }
+            .map { zipTree(it) }
+    }) {
+        exclude("META-INF/*.SF")
+        exclude("META-INF/*.DSA")
+        exclude("META-INF/*.RSA")
+    }
+
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
